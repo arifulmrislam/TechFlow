aede8d6bd1b876e6a0259f77b3447b4d062cc469
arifulmrislam <ariful.mr.islam@gmail.com>
2025-07-21 12:46:40 -0500

    Add buttons for blogs and bookmarks section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
     return (
@@ -11,7 +11,7 @@ const Hero = () => {
         >
             <div className='hero-overlay'></div>
             <div className='hero-content text-neutral-content text-center'>
-                <div className='max-w-md'>
+                <div className='max-w-lg'>
                     <h1 className='text-5xl font-bold'>
                         Welcome to
                         <span className='bg-gradient-to-r from-primary via-blue-500 to-secondary bg-300% text-transparent bg-clip-text animate-gradient'>
@@ -20,11 +20,30 @@ const Hero = () => {
                         </span>
                     </h1>
                     <p className='py-6'>
-                        TechFlow is the bridge between the complex world of technology
-                        and the curious minds eager to understand it
+                        TechFlow is the bridge between the complex world of technology and
+                        the curious minds eager to understand it
                     </p>
-                    <button className='btn btn-primary'>Read Blogs</button>
-                    <button className='btn btn-primary'>Bookmarks</button>
+                    <div className='flex gap-2 justify-center'>
+
+                        <Link to='/blogs'
+                            className='px-5 py-2.5 relative rounded group font-medium text-white font-medium inline-block'
+                        >
+                            <span className='absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500'></span>
+                            <span className='h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500'></span>
+                            <span className='absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500'></span>
+                            <span className='absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500'></span>
+                            <span className='relative'>Read Blogs</span>
+                        </Link>
+                        <Link to='/bookmarks'
+                            className='px-5 py-2.5 relative rounded group font-medium text-white font-medium inline-block'
+                        >
+                            <span className='absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500'></span>
+                            <span className='h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500'></span>
+                            <span className='absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500'></span>
+                            <span className='absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500'></span>
+                            <span className='relative'>Bookmarks</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
