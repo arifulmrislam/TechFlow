4e18499ed34b5901eaef8f2daa461570126bb8a9
arifulmrislam <ariful.mr.islam@gmail.com>
2025-08-03 08:45:42 -0500

    fix: bookmarks page and footer components

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { deleteBlog, getBlogs } from '../utils';
 import BlogCard from './../components/BlogCard';
+import EmptyState from '../components/EmptyState';
 
 
 const Bookmarks = () => {
@@ -16,8 +17,10 @@ const Bookmarks = () => {
         setBlogs(storedBlogs);
     };
 
+    if(blogs.length < 1) return <EmptyState message='No Bookmarks Available!' address={'/blogs'} label={'Go to Blogs'}/>
+
     return (
-        <div className='grid px-4 sm:px-8 lg:px-12 py- 8 justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+        <div className='grid px-4 sm:px-8 lg:px-12 py- 8 justify-center grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
             {blogs.map((blog) => (
                 <BlogCard 
                 handleDelete={handleDelete}
